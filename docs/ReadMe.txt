64ba5073f81161f0a3b478c0369493a21be34cd6
francis <francis@ebi.ac.uk>
2013-10-25 15:55:46 +0100

    Enhanced documentation

--- docs/ReadMe.txt
+++
@@ -1,3 +0,0 @@
-ipython notebook 1> notebook.log 2>&1
-
-./slideshow.sh
--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 import os, sys
 import logging
-
 from IPython.display import HTML
-
+from jinja2 import Template
 import pandas as pd
-
 from rdkit import Chem, rdBase, DataStructs
 from rdkit.Chem import AllChem, PandasTools, Draw
 from rdkit.Chem.Draw import IPythonConsole
-
 IPythonConsole.molSize = (450, 200)
