6532411091f6dabebd74a1711ffb4201ea5fb034
francis <francis@ebi.ac.uk>
2013-10-29 14:41:17 +0000

    minor tidying

--- archive.sh
+++ archive.sh
@@ -17,11 +17,12 @@ rm -f docs/*.html issues/*.html test/*.html
 cd ..
 
 tar -czvf standardiser.tgz \
-	--exclude 'problems' \
+	--exclude='archive.sh' \
+	--exclude='problems' \
 	--exclude='OLD' \
-	--exclude '*.pyc' \
+	--exclude='*.pyc' \
 	--exclude='*.log' \
-	--exclude '.git*' \
+	--exclude='.git*' \
 	--exclude='.ipynb_checkpoints' \
 	--exclude='*.swp' \
 	standardiser/
